033af9f69ca6e2c9f1366223575c78f36123de9b
hygnic <hygnic@outlook.com>
2021-07-18 19:46:53 +0800

    Initial commit

---
+++ MISC1/add_windrose_plot.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# -------------------------------------------
+# Name:              add_windrose_plot
+# Author:            Hygnic
+# Created on:        2021/7/18 19:41
+# Version:           
+# Reference:         
+"""
+Description:         
+Usage:               
+"""
+# -------------------------------------------
