a0c432cc62f220c0f0b84348836ddd2842b2f0d8
hygnic <hygnic@outlook.com>
2021-12-31 15:54:47 +0800

    update Hygnic_RVT

--- Hygnic_RVT/example1.py
+++ Hygnic_RVT/example1.py
@@ -11,15 +11,14 @@ Usage:
 """
 # -------------------------------------------
 import rvt.default
-from pprint import pprint
 
 
 """--------DATA"""
 
+
 # 峨眉山附近
 raster_dataset = r"D:\MyProject\BaiduNetdiskWorkspace\1.Blog\7_可视化\000_rvt介绍\1_data\SRTMv3_1_N29E103_峨眉山附近.tif"
 
+
 """--------"""
-
-
 dem_dict = rvt.default.get_raster_arr(raster_dataset)
