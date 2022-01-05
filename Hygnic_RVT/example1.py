67bff0a8760e2899ceafb2864f9aa9a10a242340
hygnic <hygnic@outlook.com>
2022-01-05 17:17:21 +0800

    update Hygnic_RVT

--- Hygnic_RVT/example1.py
+++ Hygnic_RVT/example1.py
@@ -10,15 +10,29 @@ Description:
 Usage:               
 """
 # -------------------------------------------
-import rvt.default
+# print(ord("文"))
+
+"""--------IMPORT_SETTINGS"""
+
+import sys
+try:
+    from osgeo import gdal
+except:
+    sys.exit('ERROR: cannot find GDAL/OGR modules')
+
+print(">>"*50)
+print('Python: {}'.format(sys.version))
+print('GDAL: {}'.format(gdal.__version__))
+print(">>"*50)
+
 
 
 """--------DATA"""
 
-
 # 峨眉山附近
-raster_dataset = r"D:\MyProject\BaiduNetdiskWorkspace\1.Blog\7_可视化\000_rvt介绍\1_data\SRTMv3_1_N29E103_峨眉山附近.tif"
+# raster_dataset = r"D:\MyProject\BaiduNetdiskWorkspace\1.Blog\7_可视化\000_rvt介绍\1_data\SRTMv3_1_N29E103_峨眉山附近.tif"
 
 
 """--------"""
-dem_dict = rvt.default.get_raster_arr(raster_dataset)
+# dem_dict = rvt.default.get_raster_arr(raster_dataset)
+
